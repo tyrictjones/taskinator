08de80c81a6055e6fb4533ae2127c44cb6f69e48
Tyric Jones <tyrictjones@gmail.com>
2022-08-14 00:29:06 -0700

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -257,46 +257,16 @@ var saveTasks = function() {
 
 //load tasks from localstorage and create new elements from the contents
 var loadTasks = function() {
-    tasks = localStorage.getItem('tasks');
-    console.log(tasks);
+    var savedTasks = localStorage.getItem('tasks');
 
-    if (tasks === null) {
-        tasks = [];
+    if (savedTasks === null) {
         return false;
     }
 
-    tasks = JSON.parse(tasks);
-    console.log(tasks);
+    savedTasks = JSON.parse(savedTasks);
 
-    for (i = 0; i < tasks.length; i++) {
-        taskIdCounter = tasks[i].id;
-
-        var listItemEl = document.createElement('li');
-        listItemEl.className = 'task-item';
-        listItemEl.setAttribute('data-task-id', tasks[i].id);
-        
-        var taskInfoEl = document.createElement('div');
-        taskInfoEl.className = 'task-info';
-        taskInfoEl.innerHTML = '<h3 class = "task-name">' + tasks[i].name + '</h3><span class = "task-type">' + tasks[i].type + '</span>';
-        listItemEl.appendChild(taskInfoEl);
-
-        var taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
-
-        if (tasks[i].status === 'to do') {
-            listItemEl.querySelector('select[name="status-change"]').selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        else if (tasks[i].status === 'in progress') {
-            listItemEl.querySelector('select[name="status-change"]').selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        }
-        else if (tasks[i].status === 'completed') {
-            listItemEl.querySelector('select[name="status-change"]').selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-        taskIdCounter++;
-        console.log(listItemEl);
+    for (var i = 0; i < savedTasks.length; i++) {
+        createTaskEl(savedTasks[i]);
     }
 };
 
